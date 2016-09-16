7ffb323eef5f651f02aa57383d9a3ad886a9db8e
Brayan Cruces <brayan2259@gmail.com>
2016-09-15 23:59:00 -0500

    cabecera innecesaria

---
+++ PC1/_materiales/1.cadenas/ejercicio_02.cpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+/**
+* Ejercicio 02: Cuenta las palabras que hay en un string
+*
+*/
+
+int contarPalabras (string s){
+
+	int contador = 1, pos;
+
+	if (s.empty())//vacio
+	{
+		contador = 0;
+	}
+	else
+	{
+		pos = s.find(" ");//buscar
+		while (pos != -1)
+		{
+			contador++;
+			pos = s.find(" ", pos + 1);
+		}
+	}
+	return contador;
+
+}
+
+
+int main(){
+	string palabra = "Hola como estas";
+
+	char fraseFormat[200];
+
+	strcpy_s(fraseFormat, palabra.c_str());
+
+	cout << "Frase: " << fraseFormat<<endl;
+	cout << "Cantidad de palabras = " << contarPalabras(palabra) << endl;
+
+	system("PAUSE");
+	return 0;
+}
