d83e2903f52506f9d42703c43d260f92135804fc
Brayan Cruces <brayan2259@gmail.com>
2016-09-15 22:51:09 -0500

    Ejercicio 01 (string) Mejor explicado

--- PC1/_materiales/1.cadenas/ejercicio_01.cpp
+++ PC1/_materiales/1.cadenas/ejercicio_01.cpp
@@ -1,37 +1,48 @@
+#pragma once
 #include<iostream>
 #include<string.h>
+
 using namespace std;
 
 /**
- * Ejercicio 01: Cuenta las vocales que hay en un string
- *
- */
+* Ejercicio 01: Cuenta las vocales que hay en un string
+*
+*/
 
-int contarCaracteres(char *s, char d) {
-	int longitud = strlen(s);
+int contarCaracteres(char *oracion, char vocal) {
+	int longitud = strlen(oracion); //obtiene longitud
 	int cont = 0;
 	for (int i = 0; i < longitud; i++)
-	{
-		if (s[i] == d)
+  	{
+		//Si en la oracion encontramos la vocal solicitada, aumentamos el contador
+		if (oracion[i] == vocal)
 		{
 			cont++;
 		}
 	}
+
 	return(cont);
 }
-void contadorDeVocales(char *s, char v[]) {
-	int sizeVoc = sizeof(s) / sizeof(char);//Recordar no me olvides
+void contadorDeVocales(char *oracion, char vocales[]) {
+
+	int sizeVoc = sizeof(oracion) / sizeof(char);//Recordar no me olvides
+
+	cout << "Tamaño de sizeVoc:" << sizeVoc <<endl;
+
 	for (int i = 0; i < sizeVoc; i++)
 	{
-		int numVocales = contarCaracteres(s, v[i]);
-		cout << "numero de vocales(" << v[i] << ")=>" << numVocales << endl;
+		int numVocales = contarCaracteres(oracion, vocales[i]);
+		cout << "Numero de vocales(" << vocales[i] << ")=>" << numVocales << endl;
 	}
 }
 int main() {
+
 	//Cuenta las vocales que hay en un string (Un string es un arreglo de caracteres)
+
 	char vocales[5] = { 'a', 'e', 'i', 'o', 'u' };
-	char* palabra = (char*)("dabale arroz a la zorra el abad");
-	contadorDeVocales(palabra, vocales);
+	char* oracion = (char*)("dabale arroz a la zorra el abad");
+
+	contadorDeVocales(oracion, vocales);
 	system("pause");
 	return(0);
 }
